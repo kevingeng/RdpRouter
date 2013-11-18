62ff69f89aa75e9f7844f16a6e6f7389ac1b7e09
kevingeng <kevingeng@163.com>
2013-11-18 15:08:56 +0800

    add vs2010 .csproject and .sln

--- TcpListenerTestA/AppSettings.Designer.cs
+++ TcpListenerTestA/AppSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace TcpListenerTestA {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
     internal sealed partial class AppSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static AppSettings defaultInstance = ((AppSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new AppSettings())));
